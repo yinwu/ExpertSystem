d8486a04fc7388c351ab92e1a537f74a0f6f2ca7
yinkui <ynkuwu@163.com>
2020-06-21 18:55:51 +0800

    20200621

--- src/ExpertSystem/experts/urls.py
+++ src/ExpertSystem/experts/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('index/', views.index, name='index'),
     path('list/', views.expert_list, name='list'),
     path('detail/', views.expert_detail, name='detail'),
     path('add/', views.add_expert_req, name='add'),
