06e086f5612adb1665374133b728e4f53542c5dd
yinkui <ynkuwu@163.com>
2020-06-21 10:25:29 +0800

    0621

--- src/ExpertSystem/experts/urls.py
+++ src/ExpertSystem/experts/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('list/', views.expert_list, name='index'),
+    path('', views.index, name='index'),
+    path('list/', views.expert_list, name='list'),
     path('detail/', views.expert_detail, name='detail'),
+    path('add/', views.add_expert_req, name='add'),
 ]
--- src/ExpertSystem/experts/views.py
+++ src/ExpertSystem/experts/views.py
@@ -8,9 +8,16 @@ from django.shortcuts import render
 from experts.models import *
 
 
+def index(request):
+    return render(request, 'index.html')
+
 def expert_list(request):
     expert_list = Expert.objects.all()
     return render(request, 'expert_list_template.html', {"result":expert_list})
+
+
+def add_expert_req(request):
+    return render(request, 'add_program_template.html')
 
 def expert_detail(request, expert_id):
     context = {"name":"test_name"}
