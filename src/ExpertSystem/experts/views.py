bb461e9bffe404877269d2dc00758785ffac48e1
yinkui <ynkuwu@163.com>
2020-07-12 22:12:17 +0800

    右上角添加登陆用户信息

--- src/ExpertSystem/experts/views.py
+++ src/ExpertSystem/experts/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, redirect
 # Create your views here.
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User, Group
+from django.contrib.auth import authenticate, login, logout
 
 from django.http import HttpResponse
 from django.shortcuts import render
@@ -43,6 +44,10 @@ def update_user(expert, new_name):
 
 def login(request):
     return render(request, 'login.html')
+
+def login_out_user(request):
+    logout(request)
+    return redirect("/experts/login/")
 
 def login_validation(request):
     next_url = request.GET.get("next")
@@ -164,18 +169,6 @@ def modify_expert_req(request, expert_id):
         return redirect("/experts/list/")
         
 def search(request):
-    test_data = [
-        {"id":"1", "name":"name1", "responser":"responser1", "description":"descriptino1", "info":"info1"},
-        {"id":"2", "name":"name2", "responser":"responser2", "description":"descriptino2", "info":"info2"},
-        {"id":"3", "name":"name3", "responser":"responser3", "description":"descriptino3", "info":"info3"},
-        {"id":"4", "name":"name4", "responser":"responser4", "description":"descriptino4", "info":"info4"},
-        {"id":"5", "name":"name5", "responser":"responser5", "description":"descriptino5", "info":"info5"},
-        {"id":"6", "name":"name6", "responser":"responser6", "description":"descriptino6", "info":"info6"},
-        {"id":"7", "name":"name7", "responser":"responser7", "description":"descriptino7", "info":"info7"},
-        {"id":"8", "name":"name8", "responser":"responser8", "description":"descriptino8", "info":"info8"},
-        {"id":"9", "name":"name9", "responser":"responser9", "description":"descriptino9", "info":"info9"},
-        {"id":"10", "name":"name10", "responser":"responser10", "description":"descriptino10", "info":"info10"}
-    ]
     keyStr = request.GET.get('search_str')
     print(keyStr)
    
